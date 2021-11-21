6a6d4603cb1ed75d263b71a611531ff53b8b6126
James Millard <james.r.millard@gmail.com>
2021-11-20 22:02:42 -0500

    tesing parallax changes

--- hooks/useIntersect.tsx
+++ hooks/useIntersect.tsx
@@ -1,18 +1,35 @@
 import { useEffect, useRef, useState } from "react";
 
-export default ({ root = null, rootMargin ='0px', threshold }) => {
+export default ({ root = null, rootMargin = '0px', threshold }) => {
   const [entry, updateEntry] = useState<IntersectionObserverEntry>();
   const [node, setNode] = useState(null);
   const observer = useRef<IntersectionObserver>()
-
+  const [dist, updateDist] = useState(0);
   useEffect(
     () => {
-      
-      const observer  = new window.IntersectionObserver(([entry]) => updateEntry(entry), {
-        root,
-        rootMargin,
-        threshold
-      });
+
+      const observer = new window.IntersectionObserver(([entry]) => {
+
+        if (entry.isIntersecting) {
+          const element = entry.target as HTMLElement
+          const distanceToTop = window.pageYOffset + element.getBoundingClientRect().top;
+          const elementHeight = element.offsetHeight;
+          var scrollTop = document.documentElement.scrollTop;
+
+          
+          if (scrollTop > distanceToTop) {
+           updateDist( 1 - (scrollTop - distanceToTop) / elementHeight);
+          }
+          
+        }
+        updateEntry(entry)
+
+      },
+        {
+          root,
+          rootMargin,
+          threshold
+        });
       observer.disconnect();
 
       if (node) observer.observe(node);
@@ -22,5 +39,5 @@ export default ({ root = null, rootMargin ='0px', threshold }) => {
     [node]
   );
 
-  return {setNode, entry};
+  return { setNode, entry, dist };
 };
