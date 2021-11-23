a8625a22668e6630edab2ffa62917655a01bfb94
acdtaz <93791555+bonesxbt@users.noreply.github.com>
2021-11-23 13:27:35 +0100

    [Change before prod] Favicon / og:image URL set to beta.magnetdao.finance

--- pages/index.tsx
+++ pages/index.tsx
@@ -125,15 +125,15 @@ const Home: NextPage = () => {
           <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=5, user-scalable=1, viewport-fit=cover" />
           <meta name="description" content="Magnet DAO is a crypto incubator that uses its treasury to cultivate, incubate, and invest in early-stage crypto projects." />
-          <link rel="preload" href="../assets/fonts/lufga-bold.woff2" as="font" crossOrigin="anonymous" />
+          <link rel="preload" href="https://beta.magnetdao.finance/assets/fonts/lufga-bold.woff2" as="font" crossOrigin="anonymous" />
           <title>Magnet DAO | Revolutionizing early-stage investing</title>
 
-          <link rel="icon" type="image/svg+xml" href="../assets/img/favicon.svg" />
-          <link rel="alternate icon" href="../assets/img/favicon.ico" />
+          <link rel="icon" type="image/svg+xml" href="https://beta.magnetdao.finance/assets/img/favicon.svg" />
+          <link rel="alternate icon" href="https://beta.magnetdao.finance/assets/img/favicon.ico" />
           <meta property="og:title" content="Magnet DAO | Revolutionizing early-stage investing" />
-          <meta property="og:image" content="../assets/img/og_img.png" />
+          <meta property="og:image" content="https://beta.magnetdao.finance/assets/img/og_img.png" />
           <meta property="og:description" content="Magnet DAO is a crypto incubator that uses its treasury to cultivate, incubate, and invest in early-stage crypto projects." />
-          <meta property="og:url" content="https://magnetdao.finance" />
+          <meta property="og:url" content="https://beta.magnetdao.finance/" />
           <meta property="og:image:width" content="1200" />
           <meta property="og:image:height" content="627" />
 
@@ -141,7 +141,7 @@ const Home: NextPage = () => {
           <meta name="twitter:site" content="@magnet_dao" />
           <meta name="twitter:title" content="Magnet DAO | Revolutionizing early-stage investing" />
           <meta name="twitter:description" content="Magnet DAO is a crypto incubator that uses its treasury to cultivate, incubate, and invest in early-stage crypto projects." />
-          <meta name="twitter:image" content="../assets/img/og_img.png" />
+          <meta name="twitter:image" content="https://beta.magnetdao.finance/assets/img/og_img.png" />
         </Head>
         <Header />
         <main onLoad={()=>{startAnimations();}}>
